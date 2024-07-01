03f4f337c37b01d7e33847acb7b789a67d319400
Stupnikjs <106406099+Stupnikjs@users.noreply.github.com>
2024-07-01 13:26:34 +0200

    Update repl.go

--- repl.go
+++ repl.go
@@ -1,2 +1,20 @@
 package repl
 
+import (
+"bufio"
+"fmt"
+"io"
+"monkey/lexer"
+"monkey/token"
+)
+const PROMPT = ">> "
+func Start(in io.Reader, out io.Writer) {
+scanner := bufio.NewScanner(in)
+for {
+fmt.Printf(PROMPT)
+scanned := scanner.Scan()
+if !scanned {
+return
+}
+line := scanner.Text()
+
